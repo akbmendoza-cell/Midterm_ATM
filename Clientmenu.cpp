7b0ed522a272d2235aa6a3f8ceb33dd810be33ce
RomeoALEJANDRO17 <romeovalenciano17@gmail.com>
2026-03-09 20:41:03 +0800

    Update Clientmenu.cpp

--- Clientmenu.cpp
+++ Clientmenu.cpp
@@ -1,6 +1,6 @@
 
 void clientMenu (vector<string>& cardNumbers, 
-                 vector<string>& cardPin, 
+                 vector<string>& encodedPINs, 
                  vector<double>& balance, 
                  vector<string>& optionsBank, 
                  vector<string>& typeAccount, 
@@ -10,5 +10,32 @@ void clientMenu (vector<string>& cardNumbers,
                  vector<string>& transactionDates, 
                  vector<string>& transactionTimes)  
 { 
-              
+string cardNum;
+string pin
+int accountIndex = -1; 
+
+// To find card, if accountIndex == -1. Card not found
+
+cout << "Enter your card number: "; 
+cin >> cardNumbers; 
+
+for (int i = 0; i < cardNumbers.size(); i++) 
+{ 
+  if(cardNumbers[i] == cardNum) 
+  { 
+  accountIndex = i; 
+  break; 
+  } 
+} 
+if (accountIndex == -1) 
+{ 
+  cout << "Card not found"; 
+  return; 
+} 
+
+// Entering of Pin 
+
+cout << "Please Enter Your PIN:"; 
+cin >> pin; 
+
 
