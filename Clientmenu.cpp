085949da852f698b4a28db86e6015df1eb83c602
RomeoALEJANDRO17 <romeovalenciano17@gmail.com>
2026-03-09 20:17:33 +0800

    Clientmenu.cpp

--- Clientmenu.cpp
+++ Clientmenu.cpp
@@ -1,1 +1,14 @@
 
+void clientMenu (vector<string>& cardNumbers, 
+                 vector<string>& cardPin, 
+                 vector<double>& balance, 
+                 vector<string>& optionsBank, 
+                 vector<string>& typeAccount, 
+                 vector<string>& typeTransaction, 
+                 vector<string>& transcationAmounts, 
+                 vector<string>& transactionFees, 
+                 vector<string>& transactionDates, 
+                 vector<string>& transactionTimes)  
+{ 
+              
+
